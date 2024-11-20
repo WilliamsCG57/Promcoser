18da1823d90f5b0034a8dcc7a0697cd01b7f58b3
Joawhi <joaq11webmovil@gmail.com>
2024-11-20 15:27:24 -0500

    Eliminar OnConfiguring en DbContext

--- PromcoserDOMAIN/Data/PromcoserDbContext.cs
+++ PromcoserDOMAIN/Data/PromcoserDbContext.cs
@@ -32,10 +32,6 @@ public partial class PromcoserDbContext : DbContext
 
     public virtual DbSet<Rol> Rol { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-68O5AFS\\SQLEXPRESS;Database=PromcoserDB;Integrated Security=true;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Cliente>(entity =>
